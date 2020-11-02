3c51935015ea80eabcdf77e23a37ebf5a89ac982
Gaaloul Maha <48685916+GaaloulMaha12@users.noreply.github.com>
2020-11-02 16:05:13 +0100

    nettyoer le projet

--- CoolBooksStart-master/src/App.vue
+++ CoolBooksStart-master/src/App.vue
@@ -7,9 +7,9 @@
         <div class="section__content section__content--p35">
           <div class="header3-wrap">
             <div class="header__logo">
-              <a href="#">
+              <router-link class="nav-link" to="/home" >
                 <img src="@/assets/images/icon/logo-white.png" alt="CoolAdmin" />
-              </a>
+              </router-link>
             </div>
             <div class="header__navbar">
               <ul class="list-unstyled">
@@ -32,59 +32,24 @@
                 </li>
               </ul>
             </div>
-            <router-view></router-view>
+
 
           </div>
         </div>
       </header>
-
-      <!-- END HEADER DESKTOP-->
-
-      <!-- PAGE CONTENT-->
-      <div class="page-content--bgf7">
-
-        <!-- WELCOME-->
-        <section class="welcome p-t-10">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-12">
-                <h1 class="title-4">Titre de la section
-                </h1>
-                <hr class="line-seprate">
-              </div>
-            </div>
-          </div>
-        </section>
-        <!-- END WELCOME-->
-
-
-        <!-- Content -->
-        <section class="container">
-
-
-
-
-
-
-        </section>
-        <!-- END STATISTIC CHART-->
-
-
-      </div>
-
+<home-page></home-page>
     </div>
-
-
-
-
   </div>
 </template>
 
 <script>
 
+import HomePage from './components/HomePage.vue'
 export default {
   name: 'App',
   components: {
+    HomePage,
+
 
   }
 }
--- CoolBooksStart-master/src/components/HomePage.vue
+++ CoolBooksStart-master/src/components/HomePage.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="hello">
+    <div class="page-content--bgf7">
 
-  </div>
+      <section class="welcome p-t-10">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h1 class="title-4">Mes Livres
+              </h1>
+              <hr class="line-seprate">
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="container">
+      </section>
+    </div>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'HomePage',
   props: {
-    msg: String
+
   }
 }
 </script>
